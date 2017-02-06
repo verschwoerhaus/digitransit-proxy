a82a8616ab8af1eb2cb6e1551387ffa20b9ee6c2
Sami Siren <siren@foofactory.fi>
2017-02-06 12:18:22 +0200

    fix for linux

--- test/test.sh
+++ test/test.sh
@@ -3,8 +3,14 @@ docker build -t hsldevcom/digitransit-proxy:test .
 
 PROXIED_HOSTS=`grep proxy_pass *.conf|cut -d'/' -f3|cut -d':' -f1|uniq`
 
-#TARGETHOST=`host www.example.com|grep -v v6|rev|cut -d' '  -f1|rev`
-TARGETHOST=`ifconfig|grep inet|grep -v inet6|grep -v 127.0.0.1|cut -d' ' -f2`
+uname -a|grep -i linux > /dev/null
+IS_LINUX=$?
+
+if [[ $ISLINUX == 0 ]]; then
+  TARGETHOST=`/sbin/ifconfig|grep inet|grep -v inet6|grep -v 127.0.0.1|cut -d':' -f2|cut -d' ' -f1|head -1`
+else
+  TARGETHOST=`/sbin/ifconfig|grep inet|grep -v inet6|grep -v 127.0.0.1|cut -d' ' -f2|head -1`
+fi
 
 #construct --add-host parameters
 for HOST in $PROXIED_HOSTS;do ADDHOSTS="--add-host $HOST:$TARGETHOST $ADDHOSTS";done;
