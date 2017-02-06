3ffd0c1b45392de3616ad6a4b6a7a1411d7893fe
Samuli Heljo <samuli@samuliheljo.com>
2017-02-06 10:48:54 +0200

    choose first host

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
 cd ..
 docker build -t hsldevcom/digitransit-proxy:test .
 
 PROXIED_HOSTS=`grep proxy_pass *.conf|cut -d'/' -f3|cut -d':' -f1|uniq`
 
 #TARGETHOST=`host www.example.com|grep -v v6|rev|cut -d' '  -f1|rev`
-TARGETHOST=`ifconfig|grep inet|grep -v inet6|grep -v 127.0.0.1|cut -d' ' -f2`
+TARGETHOST=`ifconfig|grep inet|grep -v inet6|grep -v 127.0.0.1|cut -d' ' -f2|head -1`
 
 #construct --add-host parameters
 for HOST in $PROXIED_HOSTS;do ADDHOSTS="--add-host $HOST:$TARGETHOST $ADDHOSTS";done;
