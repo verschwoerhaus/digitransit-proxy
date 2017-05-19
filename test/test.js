53f0bbb9fd75dcf95c16e8f9b7e4302fb58cf6ea
Vesa Meskanen <vesa.meskanen@cgi.com>
2017-05-19 13:27:35 +0300

    Update test

--- test/test.js
+++ test/test.js
@@ -90,9 +90,9 @@ describe('api.digitransit.fi', function() {
   testProxying('api.digitransit.fi','/routing/v1/routers/finland','opentripplanner-finland:8080');
   testProxying('api.digitransit.fi','/routing/v1/routers/hsl','opentripplanner-hsl:8080');
   testProxying('api.digitransit.fi','/routing/v1/routers/waltti','opentripplanner-waltti:8080');
-  testProxying('api.digitransit.fi','/routing-data/v1/hsl/router-hsl.zip','opentripplanner-data-con-hsl:8080');
-  testProxying('api.digitransit.fi','/routing-data/v1/waltti/router-waltti.zip','opentripplanner-data-con-waltti:8080');
-  testProxying('api.digitransit.fi','/routing-data/v1/finland/router-finland.zip','opentripplanner-data-con-finland:8080');
+  testProxying('api.digitransit.fi','/routing-data/v2/hsl/router-hsl.zip','opentripplanner-data-con-hsl:8080');
+  testProxying('api.digitransit.fi','/routing-data/v2/waltti/router-waltti.zip','opentripplanner-data-con-waltti:8080');
+  testProxying('api.digitransit.fi','/routing-data/v2/finland/router-finland.zip','opentripplanner-data-con-finland:8080');
 });
 
 describe('hsl ui', function() {
