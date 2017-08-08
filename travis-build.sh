1a4035e7b8e207005295e794b672dfd9fc653596
Sami Siren <siren@foofactory.fi>
2017-08-08 11:00:57 +0300

    Update travis-build.sh
    allow github based releases

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,6 @@
 
 set -o errexit -o nounset -o xtrace
 
-test/test.sh
-
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_ID:-latest}
 DOCKER_IMAGE=$ORG/digitransit-proxy:$DOCKER_TAG
@@ -20,11 +18,24 @@ docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   docker login -u ${DOCKER_USER} -p ${DOCKER_AUTH}
-  echo Pushing container: ${DOCKER_IMAGE}
-  docker push ${DOCKER_IMAGE}
-  echo Pushing container: ${LATEST_IMAGE}
-  docker tag ${DOCKER_IMAGE} ${LATEST_IMAGE}
-  docker push ${LATEST_IMAGE}
+  if [ "$TRAVIS_TAG" ];then
+    echo "processing release $TRAVIS_TAG"
+    #release do not rebuild, just tag
+    docker pull $ORG/$DOCKER_IMAGE
+    docker tag ${DOCKER_IMAGE} ${PROD_IMAGE}
+    docker push ${PROD_IMAGE}
+  else
+    echo "processing master build $TRAVIS_COMMIT"
+    test/test.sh
+    docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
+    docker push ${DOCKER_IMAGE}
+    docker tag ${DOCKER_IMAGE} ${LATEST_IMAGE}
+    docker push ${LATEST_IMAGE}
+  fi
+else
+  echo "processing pr $TRAVIS_PULL_REQUEST"
+  test/test.sh
+  docker build  --tag=$DOCKER_IMAGE -f Dockerfile .
 fi
 
 echo Build completed
