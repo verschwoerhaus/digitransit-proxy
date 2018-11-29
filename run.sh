26bbf7c0519a986ea2f0ffda48a31436fe3b38b3
Joel Lappalainen <joel.lappalainen@cgi.com>
2018-11-29 10:26:21 +0200

    Add more ENV parameters

--- run.sh
+++ run.sh
@@ -5,7 +5,10 @@ set -e
 if [ -n "$MESOS_CONTAINER_NAME"  ]; then 
   echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf;
 fi
-
+sed -i "s/VILKKU_BASIC_AUTH/${VILKKU_BASIC_AUTH}/" /etc/nginx/external.conf
+sed -i "s/JOJO_BASIC_AUTH/${JOJO_BASIC_AUTH}/" /etc/nginx/external.conf
+sed -i "s/LAPPEENRANTA_BASIC_AUTH/${LAPPEENRANTA_BASIC_AUTH}/" /etc/nginx/external.conf
+sed -i "s/LINKKI_BASIC_AUTH/${LINKKI_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/LISSU_BASIC_AUTH/${LISSU_BASIC_AUTH}/" /etc/nginx/external.conf
 
 #start nginx
